0f1948f8c46b3672ac89c13dcddfff1f5067d6ab
Robert Fisk <robert_fisk@fastmail.fm>
2018-04-01 00:23:38 +1300

    Tweak HID protocol defines

--- Upstream/Middlewares/ST/STM32_USB_Device_Library/Class/HID/Inc/usbd_hid.h
+++ Upstream/Middlewares/ST/STM32_USB_Device_Library/Class/HID/Inc/usbd_hid.h
@@ -74,8 +74,8 @@
 #define HID_REQ_SET_REPORT            0x09
 #define HID_REQ_GET_REPORT            0x01
 
-#define HID_KEYBRD_BOOT_CODE          0x01
-#define HID_MOUSE_BOOT_CODE           0x02
+#define HID_KEYBRD_PROTOCOL          0x01
+#define HID_MOUSE_PROTOCOL           0x02
 
 /**
   * @}
